c4096eb2d89f30ce47f2e0bf0dd01271e5bddb09
Yasin <yasinerdemli35@icloud.com>
2022-05-11 01:55:11 +0300

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Yasin Erdemli on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
